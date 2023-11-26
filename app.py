b21d4bd3bf926b52c19d153c1af559b0b88bea43
ShorenaK <shorenaanzhilov@gmail.com>
2023-11-26 16:40:48 -0500

    Global varibales created

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
-
+from random import randint
 
 
 app = Flask(__name__)
 
-
-
+# Initialize global variable
+CHIPS = 50
+SUM = 0
+SUM_DL_CARDS = 0 
+HAS_BLACKJACK = False
+IS_IN_GAME = False
+MESSAGE_DL = []
+MASSAGE = ''
+CARD_DECK = []
+CARD_DECK_DL = []
+PLAYER = {
+    'name': 'Player',
+    'chips': 1000
+}
 
 
     
