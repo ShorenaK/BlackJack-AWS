a4c6b4636f0a4d96909e4dcc5191dd72f954673c
ShorenaK <shorenaanzhilov@gmail.com>
2023-12-04 14:54:13 -0500

    cleaned file

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from random import randint
 app = Flask(__name__)
 
 class BlackjackGame:
+    ''' Class represeting a Blackjack game. '''
     def __init__(self):
+        ''' Initialize the BalckjackGame Instance. '''
         self.CHIPS = 50
         self.SUM_PL_CARDS= 0
         self.SUM_DL_CARDS = 0 
@@ -14,10 +16,8 @@ class BlackjackGame:
         self.MESSAGE = ''
         self.CARD_DECK_Pl = []
         self.CARD_DECK_Dl = []
-        self.PLAYER = {
-            'name': 'Player',
-            'chips': 1000
-        }
+        self.PLAYER = {'name': 'Player', 'chips': 1000}
+
 
     def shuffle_new_card(self):
         '''
@@ -25,6 +25,7 @@ class BlackjackGame:
     
         Returns:
         int: A random card value between 1 and 11. 
+        
         '''
         random_num = randint(1, 13)
         if random_num > 10:
@@ -38,6 +39,10 @@ class BlackjackGame:
     def start_game(self):
         '''
         Starts the game by initializing player's and dealer's cards.
+        
+        Returns:
+        jsonify: JSON response with game sate.
+        
         '''
         
         self.IS_IN_GAME = True
@@ -55,7 +60,7 @@ class BlackjackGame:
         self.SUM_DL_CARDS = sum(self.CARD_DECK_Dl)
         self.CARD_DECK_Dl = f"{card_one_dl}  {card_two_dl}"
         
-        # Return a JSON response with game state
+        # Return a JSON response with game state.
         return jsonify({
             'CARD_DECK_Pl': self.CARD_DECK_Pl,
             'CARD_DECK_Dl': self.CARD_DECK_Dl,
@@ -71,37 +76,36 @@ class BlackjackGame:
     def render_game(self):
         '''
         Renders the game state, updates messages, and displays cards.
+        
+        Retunrs:
+        jsonify: JSON resonse with updated game state.
+        
         '''
-
 
         total_dl_tag = f'Total: {self.SUM_DL_CARDS}'
         total_pl_tag = f'Total: {self.SUM_PL_CARDS}'
         
-        # Update messages based on game logic
+        # Update messages based on game logic.
         if self.SUM_DL_CARDS == 21:
-            self.MESSAGE_DL = "Table Wins Black Jack!"
-            self.MESSAGE = "Player lost!"
-            self.PLAYER['chips'] -= self.CHIPS
-            self.IS_IN_GAME = False
-            
+           self.MESSAGE_DL = "Table Wins Black Jack!"
+           self.MESSAGE = "Player lost!"
+           self.PLAYER['chips'] -= self.CHIPS
+           self.IS_IN_GAME = False   
         elif self.SUM_PL_CARDS == 21:
-            self.MESSAGE = "Player wins Black Jack!!!"
-            self.PLAYER['chips'] += self.CHIPS
-            self.HAS_BLACKJACK = True
-            
+             self.MESSAGE = "Player wins Black Jack!"
+             self.PLAYER['chips'] += self.CHIPS
+             self.HAS_BLACKJACK = True  
         elif self.SUM_PL_CARDS == self.SUM_DL_CARDS:
-            self.MESSAGE = "It's TIE"
-            
+             self.MESSAGE = "It's TIE"  
         elif self.SUM_PL_CARDS <= 20:
-            self.IS_IN_GAME = True
-            self.MESSAGE = "Would you like to hit?"
-            
+             self.IS_IN_GAME = True
+             self.MESSAGE = "Would you like to hit?"   
         elif self.SUM_PL_CARDS > 21:
-            self.MESSAGE = "Bust! You lost a Bet!"
-            self.PLAYER['chips'] -= self.CHIPS
-            self.IS_IN_GAME = False
+             self.MESSAGE = "Bust! You lost a Bet!"
+             self.PLAYER['chips'] -= self.CHIPS
+             self.IS_IN_GAME = False
         
-        # Return HTML content as a response
+        # Return HTML content as a response.
         return jsonify({
             'CARD_DECK_Pl': self.CARD_DECK_Pl,
             'CARD_DECK_Dl': self.CARD_DECK_Dl,
@@ -117,9 +121,13 @@ class BlackjackGame:
     def new_card(self):
         '''
         Draws a new card for the player and updates the game state.
+        
+        Retunrs:
+        jsonify: JSON response with updated game state.
+        
         '''
         
-        # Default value of new card
+        # Default value of new card.
         player_card = None
         
         if self.IS_IN_GAME and not self.HAS_BLACKJACK:
@@ -138,19 +146,20 @@ class BlackjackGame:
             
         })
 
-# Flask route for rendering the game page
+# Flask route for rendering the game page.
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Flask route for rendering the main casino page
+# Flask route for rendering the main casino page.
 @app.route('/indexmain')
 def indexmain():
     return render_template('indexmain.html')
 
 # Create an instance of the BlackJackGame class.
 blackjack_game = BlackjackGame()
-# Flask route for handling game actions
+
+# Flask route for handling game actions.
 @app.route('/game_action/<action>')
 def game_action(action):
     '''
@@ -159,21 +168,19 @@ def game_action(action):
     Args:
         action (str): The action to perform ('startTheGame' or 'newCard')
         
-        Returns:
-            str: HTML page with the updated game state.
+    Returns:
+        str: HTML page with the updated game state.
+        
      '''
 
     if action == 'startTheGame':
        blackjack_game.start_game()
-        
     elif action == 'newCard':
          blackjack_game.new_card()
     
     # Call render_game to get updated game state
-    render_content = blackjack_game.render_game()
-         
+    render_content = blackjack_game.render_game()  
     return render_content
             
-
 if __name__ == "__main__":
     app.run(port=8008, debug=True, use_reloader=True)
