f2e5cfdfcca33454e5194087e84c7c814a056067
ShorenaK <shorenaanzhilov@gmail.com>
2023-12-04 16:48:13 -0500

    test_balckjack.py completed

--- test_blackjack.py
+++ test_blackjack.py
@@ -1,5 +1,6 @@
 import unittest
-from app.py import BlackjackGame
+from app import BlackjackGame
+
 
 class TestBlackjackGame(unittest.TestCase):
     def setUp(self):
@@ -10,19 +11,20 @@ class TestBlackjackGame(unittest.TestCase):
         '''Test the shuffle_new_card function.'''
         card = self.blackjack_game.shuffle_new_card()
         self.assertTrue(1 <= card <= 11, " A card value should be between 1 and 11." )
-        
+
+
     def test_start_game(self):
         '''Test the start_game function.'''
         # Call the start_game function to initialize the game.
         game_state = self.blackjack_game.start_game()
         
         #  Assertions based on the expected initilial game state.
-        self.assertTrue(game_state['IS IN GAME'], "Game should ne in progress.")
+        self.assertTrue(game_state['IS_IN_GAME'], "Game should ne in progress.")
         self.assertEqual(game_state['MESSGE'], "", "Initial message should be empty.")
-        self.assertEqual(game_state(['SUM_DL_CARDS'], 0, "Dealer's total shoudl be 0."))
-        self.assertEqual(game_state(['SUM_PL_CARDS']), 0, "Plyer's total shoudl be 0." )
-        
-        
+        self.assertEqual(game_state['SUM_DL_CARDS'], 0, "Dealer's total shoudl be 0.")
+        self.assertEqual(game_state['SUM_PL_CARDS'], 0, "Plyer's total shoudl be 0." )
+
+
     def test_render_game(self):
         '''Test the render function'''
         # Set up the game state.
@@ -38,4 +40,24 @@ class TestBlackjackGame(unittest.TestCase):
         self.assertEqual(game_state['SUM_PL_CARDS'], 15, "Player's total should be 15." )
         self.assertEqual(game_state['MESSAGE_DL'], "", "Dealer's message should be empty.")
         self.assertEqual(game_state['MESSAGE'], "Would you like to hit?", "Player's message should be 'Would you like to hit?'.")
+
+
+    def test_new_card(self):
+        '''Test the new_card funtion'''
+
+        # Set up the game state.
+        self.blackjack_game.IS_IN_GAME = True
+        self.blackjack_game.HAS_BLACKJACK = False
+        self.blackjack_game.SUM_PL_CARDS = 18 
         
+        # Call new_card to draw a new card
+        game_state = self.blackjack_game.new_card()
+        
+        # Assertions based on the expected new game state.
+        self.assertEqual(len(game_state['CARD_DECK_Pl']), 1, "One new card should be added to the player's deck.")
+        self.assertEqual(game_state['SUM_PL_CARDS'],18 + game_state['CARD_DECK_Pl'][0], "Player's total should be updated correctly.")
+        self.assertEqual(game_state['MESSAGE'],"", "Player's message should be empty." )
+        self.assertTrue(game_state['IS_IN_GAME'], "Game should still be in progress." )
+        
+if __name__ == '__main':
+    unittest.main()
